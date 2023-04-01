de54d25063650955a7d7a716e51a91eed342c3ad
Frangiskos <frangiskos@gmail.com>
2023-04-01 15:46:42 +0300

    feat: get account balance

--- solana-intro-frontend-starter/pages/index.tsx
+++ solana-intro-frontend-starter/pages/index.tsx
@@ -2,14 +2,36 @@ import type { NextPage } from 'next';
 import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import AddressForm from '../components/AddressForm';
+import * as Web3 from '@solana/web3.js';
 
 const Home: NextPage = () => {
     const [balance, setBalance] = useState(0);
     const [address, setAddress] = useState('');
+    const [isExecutable, setIsExecutable] = useState<null | boolean>(null);
+    const [accountInfo, setAccountInfo] = useState<null | Web3.AccountInfo<any>>(null);
 
     const addressSubmittedHandler = (address: string) => {
-        setAddress(address);
-        setBalance(1000);
+        try {
+            setAddress(address);
+            const key = new Web3.PublicKey(address);
+            const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
+            connection.getBalance(key).then((balance) => {
+                setBalance(balance / Web3.LAMPORTS_PER_SOL);
+            });
+            connection.getAccountInfo(key).then((accountInfo) => {
+                setAccountInfo(accountInfo);
+                console.log(accountInfo);
+                if (accountInfo === null) {
+                    setIsExecutable(null);
+                } else {
+                    setIsExecutable(accountInfo.executable);
+                }
+            });
+        } catch (error) {
+            setAddress('');
+            setBalance(0);
+            alert(error);
+        }
     };
 
     return (
@@ -19,6 +41,7 @@ const Home: NextPage = () => {
                 <AddressForm handler={addressSubmittedHandler} />
                 <p>{`Address: ${address}`}</p>
                 <p>{`Balance: ${balance} SOL`}</p>
+                <p>{`Is Executable: ${isExecutable === null ? '-' : isExecutable}`}</p>
             </header>
         </div>
     );
