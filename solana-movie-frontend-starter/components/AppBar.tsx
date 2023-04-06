a463299b44ceed48be9ddb519338c86293c85872
Frangiskos <frangiskos@gmail.com>
2023-04-06 22:17:35 +0300

    linting update

--- solana-movie-frontend-starter/components/AppBar.tsx
+++ solana-movie-frontend-starter/components/AppBar.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react'
-import styles from '../styles/Home.module.css'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import Image from 'next/image'
+import { FC } from 'react';
+import styles from '../styles/Home.module.css';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import Image from 'next/image';
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
+            <Image alt="logo" src="/solanaLogo.png" height={30} width={200} />
             <span>Movie Reviews</span>
             <WalletMultiButton />
         </div>
-    )
-}
+    );
+};
--- solana-movie-frontend-starter/components/Card.tsx
+++ solana-movie-frontend-starter/components/Card.tsx
@@ -1,4 +1,5 @@
 import { Box, HStack, Spacer, Stack, Text } from '@chakra-ui/react';
+
 import { FC } from 'react';
 import { Movie } from '../models/Movie';
 
@@ -8,22 +9,16 @@ export interface CardProps {
 
 export const Card: FC<CardProps> = (props) => {
     return (
-        <Box
-            p={4}
-            display={{ md: "flex" }}
-            maxWidth="32rem"
-            borderWidth={1}
-            margin={2}
-        >
+        <Box p={4} display={{ md: 'flex' }} maxWidth="32rem" borderWidth={1} margin={2}>
             <Stack
-                w='full'
-                align={{ base: "center", md: "stretch" }}
-                textAlign={{ base: "center", md: "left" }}
+                w="full"
+                align={{ base: 'center', md: 'stretch' }}
+                textAlign={{ base: 'center', md: 'left' }}
                 mt={{ base: 4, md: 0 }}
                 ml={{ md: 6 }}
                 mr={{ md: 6 }}
             >
-                <HStack >
+                <HStack>
                     <Text
                         fontWeight="bold"
                         textTransform="uppercase"
@@ -34,17 +29,12 @@ export const Card: FC<CardProps> = (props) => {
                         {props.movie.title}
                     </Text>
                     <Spacer />
-                    <Text
-                        color="gray.200"
-                    >
-                        {props.movie.rating}/5
-                    </Text>
+                    <Text color="gray.200">{props.movie.rating}/5</Text>
                 </HStack>
                 <Text my={2} color="gray.400">
                     {props.movie.description}
                 </Text>
             </Stack>
         </Box>
-    )
-}
-
+    );
+};
